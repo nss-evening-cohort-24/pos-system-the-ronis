19ef9b26f1a9a15663e18ebe9841f9676372710c
Ryan Shore <mshorecode@gmail.com>
2023-09-11 23:58:00 -0500

    Added utility functions

---
+++ utils/clearDom.js
@@ -0,0 +1,7 @@
+const clearDom = () => {
+  document.querySelector('#button-container').innerHTML = '';
+  document.querySelector('#form-container').innerHTML = '';
+  document.querySelector('#orders').innerHTML = '';
+};
+
+export default clearDom;
