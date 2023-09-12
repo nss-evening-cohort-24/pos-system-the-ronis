e8f06903685eb72582fc3bdc0f41756aac9393a5
Keana Cobarde <keanacobarde@gmail.com>
2023-09-12 17:46:16 -0500

    modifying body color and button text

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<div class="login-screen"><div class="login-header"><img src="https://i.imgur.com/i130ILY.png"><div><div class=login-button><button type="button" class="btn btn-success btn-lg" id="login-button"> Log In! </button></div></div><div></div>';
+  const domString = '<div class="login-screen"><div class="login-header"><img src="https://i.imgur.com/i130ILY.png"><div><div class=login-button><button type="button" class="btn btn-success btn-lg" id="login-button"> Log In and Get Started </button></div></div><div></div>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#login-button').addEventListener('click', signIn);
 };
