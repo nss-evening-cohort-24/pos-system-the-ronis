996ccee27e67fb0e87230618af01379ab477ac47
Keana Cobarde <keanacobarde@gmail.com>
2023-09-12 19:27:27 -0500

    changing method within home.js so that display name could be displayed

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import renderToDOM from '../utils/renderToDom';
 const homePage = (user) => {
   let domString = '';
 
-  domString += `<h1> Welcome, ${user.name}</h1>
+  domString += `<h1> Welcome, ${user.displayName}!</h1>
   <button type="button" id ='home-view-orders-btn' class="btn btn-success">View Orders</button>
   <button type="button" id ='home-create-orders-btn' class="btn btn-info">Create An Order</button>
   <button type="button" id ='home-view-revenue-btn' class="btn btn-warning">View Revenue</button>
