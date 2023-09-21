e348804d258b393a339204e198a49432205a0931
Shari Ebach <ebachshari@yahoo.com>
2023-09-21 17:50:24 -0500

    menu items CRUD and search bar finished

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,4 +1,4 @@
-import { getOrders } from '../api/ordersData';
+import { getOrders, searchOrders } from '../api/ordersData';
 import addOrderForm from '../components/forms/addOrderForm';
 import { emptyOrders, showOrders } from '../pages/orders';
 
@@ -16,6 +16,22 @@ const navigationEvents = (user) => {
   document.querySelector('#create-order').addEventListener('click', () => {
     addOrderForm();
   });
+
+  document.querySelector('#searchBar').addEventListener('keyup', (e) => {
+    const searchValue = document.querySelector('#searchBar').value.toLowerCase();
+
+    if (e.keyCode === 13) {
+      searchOrders(searchValue, user.uid)
+        .then((search) => {
+          if (search.length) {
+            showOrders(search);
+          } else {
+            emptyOrders();
+          }
+        });
+    }
+  });
+  document.querySelector('#search').value = '';
 };
 
 export default navigationEvents;
