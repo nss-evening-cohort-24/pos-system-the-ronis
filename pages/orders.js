223bc2e25680402124790f4d08d7a3f9039c375c
Keana Cobarde <keanacobarde@gmail.com>
2023-09-23 11:43:34 -0500

    filtered wiuthin orders.js so that reg users could only see and edit orders created by them

--- pages/orders.js
+++ pages/orders.js
@@ -10,37 +10,60 @@ const emptyOrders = () => {
 const showOrders = (array, user) => {
   let domString = '';
   clearDom();
-  if (array.length < 1) {
-    emptyOrders();
-  } else {
-    domString = '';
-    array.forEach((item) => {
-      domString += `
-            <div class="orderCard card">
-              <div class="card-body cardStyle" style="height: 280px; width: 200px;">
-              <div>
-                  <h5 class="card-title">${item.name}</h5>
-                    <p class="card-text bold">${item.status ? 'closed' : 'open'}</p>
-                    <p class="card-text bold">${item.phone}</p>
-                    <p class="card-text emailSmall bold">${item.email}</p>
-                    <p class="card-text bold">${item.ordertype}</p>
-      <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>`;
-      admin.forEach((au) => {
-        if (au === user.uid) {
+  admin.forEach((adminuser) => {
+    if (adminuser === user.uid) {
+      // render all cards wth all buttons
+      if (array.length < 1) {
+        emptyOrders();
+      } else {
+        domString = '';
+        array.forEach((item) => {
+          domString += `
+                <div class="orderCard card">
+                  <div class="card-body cardStyle" style="height: 280px; width: 200px;">
+                  <div>
+                      <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text bold">${item.status ? 'closed' : 'open'}</p>
+                        <p class="card-text bold">${item.phone}</p>
+                        <p class="card-text emailSmall bold">${item.email}</p>
+                        <p class="card-text bold">${item.ordertype}</p>
+          <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>`;
           domString += `${item.status ? ''
             : `<a href="#" id='order-edit--${item.firebaseKey}' class="card-link">Edit</a>
-      <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}`;
-        } else {
-          domString += '';
-        }
-      });
-      domString += `</div>
-      </div>
-      </div>`;
-    });
-
-    renderToDOM('#orders', domString);
-  }
+          <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}`;
+          domString += `</div>
+          </div>
+          </div>`;
+        });
+      }
+    } else {
+      const nonAdminOrders = array.filter((orders) => orders.user === user.uid);
+      if (nonAdminOrders.length < 1) {
+        emptyOrders();
+      } else {
+        domString = '';
+        nonAdminOrders.forEach((item) => {
+          domString += `
+                <div class="orderCard card">
+                  <div class="card-body cardStyle" style="height: 280px; width: 200px;">
+                  <div>
+                      <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text bold">${item.status ? 'closed' : 'open'}</p>
+                        <p class="card-text bold">${item.phone}</p>
+                        <p class="card-text emailSmall bold">${item.email}</p>
+                        <p class="card-text bold">${item.ordertype}</p>
+          <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>`;
+          domString += `${item.status ? ''
+            : `<a href="#" id='order-edit--${item.firebaseKey}' class="card-link">Edit</a>
+          <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}`;
+          domString += `</div>
+          </div>
+          </div>`;
+        });
+      }
+    }
+  });
+  renderToDOM('#orders', domString);
 };
 
 export { showOrders, emptyOrders };
