77eed499c39723e4e66fbbedee112031c6a4cfb1
Ryan Shore <mshorecode@gmail.com>
2023-09-23 01:08:18 -0500

    edited orders css

--- pages/orders.js
+++ pages/orders.js
@@ -14,20 +14,24 @@ const showOrders = (array) => {
     let domString = '';
     array.forEach((item) => {
       domString += `
-        <div class="card-body card-style" style="height: 350px; width: 350px;">
+        <div class="card-style">
           <div class="order-title">
             <h5 class="order-name">${item.name}</h5>
+            <p id="${item.status === false ? 'order-open' : 'order-closed'}" class="card-text order-status bold">${item.status ? 'closed' : 'open'}</p>
           </div>
           <div class="order-info">
-            <p class="card-text order-status bold">${item.status ? 'closed' : 'open'}</p>
-            <p class="card-text bold">Phone: ${item.phone}</p>
-            <p class="card-text emailSmall bold">Email: ${item.email}</p>
-            <p class="card-text bold">Type: ${item.ordertype}</p>
+            <p class="card-text bold">${item.phone}</p>
+            <p class="card-text emailSmall bold">${item.email}</p>
+            <p class="card-text bold">${item.ordertype}</p>
           </div>
-          <div class="order-buttons">
-            <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>
-            ${item.status ? '' : `<a href="#" id='order-edit--${item.firebaseKey}' class="card-link">Edit</a>
-            <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}
+          <div id="order-buttons">
+            <div id="details">
+              <a href="#" id='order-details--${item.firebaseKey}' class="order-details">Details</a>
+            </div>
+            <div id="edit-delete">
+              ${item.status ? '' : `<a href="#" id='order-edit--${item.firebaseKey}' class="order-edit">Edit</a>
+              <a href="#" i id='order-delete--${item.firebaseKey}' class="order-delete fa-solid fa-trash-can" /></a>`}    
+            </div>
           </div>
         </div>`;
     });
