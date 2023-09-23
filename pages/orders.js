529fc501065983d5b6f4f2a33f1f6bc1b878ccec
Ryan Shore <mshorecode@gmail.com>
2023-09-22 22:56:29 -0500

    styling

--- pages/orders.js
+++ pages/orders.js
@@ -14,22 +14,22 @@ const showOrders = (array) => {
     let domString = '';
     array.forEach((item) => {
       domString += `
-        <div class="orderCard card">
-          <div class="card-body cardStyle" style="height: 280px; width: 200px;">
-          <div>
-              <h5 class="card-title">${item.name}</h5>
-                <p class="card-text bold">${item.status ? 'closed' : 'open'}</p>
-                <p class="card-text bold">${item.phone}</p>
-                <p class="card-text emailSmall bold">${item.email}</p>
-                <p class="card-text bold">${item.ordertype}</p>
-                <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>
-                  ${item.status ? ''
-    : `<a href="#" id='order-edit--${item.firebaseKey}' class="card-link">Edit</a>
-                  <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}
-            </div>
+        <div class="card-body card-style" style="height: 350px; width: 350px;">
+          <div class="order-title">
+            <h5 class="order-name">${item.name}</h5>
           </div>
-        </div>
-        `;
+          <div class="order-info">
+            <p class="card-text order-status bold">${item.status ? 'closed' : 'open'}</p>
+            <p class="card-text bold">Phone: ${item.phone}</p>
+            <p class="card-text emailSmall bold">Email: ${item.email}</p>
+            <p class="card-text bold">Type: ${item.ordertype}</p>
+          </div>
+          <div class="order-buttons">
+            <a href="#" id='order-details--${item.firebaseKey}' class="card-link">Details</a>
+            ${item.status ? '' : `<a href="#" id='order-edit--${item.firebaseKey}' class="card-link">Edit</a>
+            <a href="#" i id='order-delete--${item.firebaseKey}' class="fa-solid fa-trash-can" /></a>`}
+          </div>
+        </div>`;
     });
     renderToDOM('#orders', domString);
   }
